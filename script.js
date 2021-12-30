4c44b911623d95709037ad68034f70868074e571
Patrycja <pwbanaszuk@gmail.com>
2021-12-30 22:18:19 +0000

    added popup and control button inside <form>, change opup size, added x3 functions and x2 addEventListeners

---
+++ script.js
@@ -0,0 +1,54 @@
+const username = document.querySelector('#username')
+const pass = document.querySelector('#upassword')
+const pass2 = document.querySelector('#password2')
+const email = document.querySelector('#email')
+const sendBtn = document.querySelector('.send')
+const clearBtn = document.querySelector('.clear')
+const popup = document.querySelector('.popup')
+
+
+
+const showError = (input, msg) => {
+
+    //argument input przechowuje nasze elementy
+    //argument msg przechowuje placeholder
+
+    const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error.text')
+
+
+    formBox.classList.add('error')
+    errorMsg.textContent = msg
+    
+}
+
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
+}
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			clearError(el)
+		}
+	})
+}
+
+//argument input z funkcji "checkForm", przechowuję tablicę z naszymi inputami
+//arument el odnosi sie do każdej zmiennej, która umieśclismy w tablicy
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault()
+
+	checkForm([username, pass, pass2, email])
+})
+
+clearBtn.addEventListener('click', e => {
+	e.preventDefault()
+	[(username, pass, pass2, email)].forEach(el => {
+		el.value = ''
+	})
+})
