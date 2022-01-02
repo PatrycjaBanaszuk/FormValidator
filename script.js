6aac08ec101a89da683b3b9ffdd1e0649957b0f2
Patrycja <pwbanaszuk@gmail.com>
2022-01-02 22:22:49 +0000

    added function checkErrors in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pass2 = document.querySelector('#password2')
 const email = document.querySelector('#email')
 const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
-const popup = document.querySelector('.popup')
+const popup = document.querySelector('.pop-up')
 
 const showError = (input, msg) => {
 	//argument input przechowuje nasze elementy
@@ -49,17 +49,34 @@ const checkPassword = (pass1, pass2) => {
 	}
 }
 
-const checkMial = email => {
-
+const checkMail = email => {
 	const re =
-	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
-	if(re.test(email.value)) {
+	if (re.test(email.value)) {
 		clearError(email)
 	} else {
 		showError(email, 'Email is incorrect!')
 	}
 }
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0
+
+	allInputs.forEach(el => {
+		if(el.classList.contains('error')){
+			errorCount++
+		}
+	});
+
+	if(errorCount === 0){
+		popup.classList.add('show-popup')
+	}
+	
+	console.log(errorCount);
+}
+
 
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
@@ -69,12 +86,16 @@ sendBtn.addEventListener('click', e => {
 	checkLength(username, 3)
 	checkLength(pass, 8)
 	checkPassword(pass, pass2)
-	checkMial(email)
+	checkMail(email)
+	checkErrors()
 })
 
 clearBtn.addEventListener('click', e => {
-	e.preventDefault();
-	[username, pass, pass2, email].forEach(el => {
+	e.preventDefault()
+	;[username, pass, pass2, email].forEach(el => {
 		el.value = ''
+		clearError(el)
 	})
 })
+
+
