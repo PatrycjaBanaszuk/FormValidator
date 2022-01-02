8f8e45dbea3c63c0747aae4422bd413d5c97568a
Patrycja <pwbanaszuk@gmail.com>
2022-01-02 21:03:48 +0000

    added x2 new functions to script.js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const username = document.querySelector('#username')
-const pass = document.querySelector('#upassword')
+const pass = document.querySelector('#password')
 const pass2 = document.querySelector('#password2')
 const email = document.querySelector('#email')
 const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
-
-
 const showError = (input, msg) => {
+	//argument input przechowuje nasze elementy
+	//argument msg przechowuje placeholder
 
-    //argument input przechowuje nasze elementy
-    //argument msg przechowuje placeholder
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error-text')
 
-    const formBox = input.parentElement
-	const errorMsg = formBox.querySelector('.error.text')
-
-
-    formBox.classList.add('error')
-    errorMsg.textContent = msg
-    
+	formBox.classList.add('error')
+	errorMsg.textContent = msg
 }
 
 const clearError = input => {
@@ -40,15 +35,46 @@ const checkForm = input => {
 //argument input z funkcji "checkForm", przechowuję tablicę z naszymi inputami
 //arument el odnosi sie do każdej zmiennej, która umieśclismy w tablicy
 
+const checkLength = (input, min) => {
+	// console.log(min);
+	// console.log(input);
+	if (input.value.length < min) {
+		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} must contain ${min} characters.`)
+	}
+}
+
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, 'Passwords do not match!')
+	}
+}
+
+const checkMial = email => {
+
+	const re =
+	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+	if(re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'Email is incorrect!')
+	}
+}
+
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
 
 	checkForm([username, pass, pass2, email])
+
+	checkLength(username, 3)
+	checkLength(pass, 8)
+	checkPassword(pass, pass2)
+	checkMial(email)
 })
 
 clearBtn.addEventListener('click', e => {
-	e.preventDefault()
-	[(username, pass, pass2, email)].forEach(el => {
+	e.preventDefault();
+	[username, pass, pass2, email].forEach(el => {
 		el.value = ''
 	})
 })
